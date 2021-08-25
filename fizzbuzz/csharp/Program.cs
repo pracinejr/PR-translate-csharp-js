2188a878c9460a991adc14b57eeb1549843aca27
Peter Racine Jr <peterracinejr@gmail.com>
2021-08-25 16:08:15 -0500

    finished translating fizzbuzz

---
+++ fizzbuzz/csharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i=1; i<= 100; i++) 
+            {
+                if (i % 15 == 0) 
+            {
+                Console.WriteLine("fizzbuzz");
+            } else if (i % 3 == 0) 
+            
+            { 
+                Console.WriteLine("fizz");
+            } else if (i % 5 == 0) {
+                Console.WriteLine("buzz");
+            } else {
+                Console.WriteLine(i);
+            }
+            }
+        }
+    }
+}
